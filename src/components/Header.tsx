af40d802d8fd2ed262a0e0f564652cfa29f867ff
tlatmdxo123 <tmdxosmswkd@gmail.com>
2022-02-02 19:15:27 +0900

    refactor:change Header Compoent Text to Text Component - 공통으로 사용하는 컴포넌트는 분리하여 공유

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NormalText, Title } from './Texts';
 
 function Header() {
     return (
         <Container>
             <Title>들어온 요청</Title>
-            <Desc>파트너님에게 딱 맞는 요청서를 찾아보세요.</Desc>
+            <NormalText>파트너님에게 딱 맞는 요청서를 찾아보세요.</NormalText>
         </Container>
     );
 }
@@ -15,15 +16,6 @@ const Container = styled.header`
     margin-bottom:32px;
 `
 
-const Title = styled.h1`
-    font-size: ${({theme}) => theme.fontSizes["xl"]};
-    color:${({theme}) => theme.colors.gray[900]};
-    font-weight: 600;
-`
-const Desc = styled.p`
-    font-size: ${({theme}) => theme.fontSizes["m"]};
-    color:${({theme}) => theme.colors.gray[900]};
-    font-weight: 400;
-`
+
 
 export default Header;
--- src/components/Texts.tsx
+++ src/components/Texts.tsx
@@ -1,19 +1,28 @@
 import styled from 'styled-components';
 
+export const Title = styled.h1`
+    font-size: ${({theme}) => theme.fontSizes["xl"]};
+    color:${({theme}) => theme.colors.gray[900]};
+    font-weight: 600;
+`
+
 export const SubTitle = styled.h4`
-    font-size: 16px;
+    font-size: ${({theme}) => theme.fontSizes['l']};
     color:${({theme}) => theme.colors.gray[900]};
     font-weight: 700;
     margin-bottom: 4px;
 `
+
 export const NormalText = styled.p`
-    font-size: 14px;
+    font-size: ${({theme}) => theme.fontSizes['m']};
     color:${({theme}) => theme.colors.gray[900]};
     font-weight: 400;
 `
+
 export const NormalTextLight = styled(NormalText)`
     color:${({theme}) => theme.colors.gray[600]};
 `
+
 export const NormalTextBold = styled(NormalText)`
     font-weight: 700;
 `
